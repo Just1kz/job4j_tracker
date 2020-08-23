532e3f964b6098e6604ee55be6158aa23733cb36
Just1kz <exelent.pride@rambler.ru>
2020-08-24 01:41:04 +0700

    3. Банковские переводы [#306901]

---
+++ src/main/java/ru/job4j/tracker/bank/BankService.java
@@ -0,0 +1,35 @@
+package ru.job4j.tracker.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (!users.containsKey(user.getPassport())) {
+            users.put(user, new ArrayList<Account>());
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+
+    }
+
+    public User findByPassport(String passport) {
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String dеstRequisite, double amount) {
+        boolean rsl = false;
+        return rsl;
+    }
+}
