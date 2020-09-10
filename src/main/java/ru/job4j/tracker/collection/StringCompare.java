74db2139ef2c417a906ca91416fe23dac7010db9
Just1kz <exelent.pride@rambler.ru>
2020-09-10 20:16:10 +0700

    4. Сортировка номера [#306886]

--- src/main/java/ru/job4j/tracker/collection/StringCompare.java
+++ src/main/java/ru/job4j/tracker/collection/StringCompare.java
@@ -7,24 +7,32 @@ import java.util.List;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        boolean a = true;
-        int count = 0;
+//        boolean a = true;
+//        int count = 0;
+//        int rsl = 0;
+//        List<Character> first = new ArrayList<>();
+//        for(Character firstInvite : left.toCharArray()) {
+//            first.add(firstInvite);
+//        }
+//        List<Character> second = new ArrayList<>();
+//        for(Character secondInvite : right.toCharArray()) {
+//            second.add(secondInvite);
+//        }
+//        do {
+//            rsl = Character.compare(first.get(count),second.get(count));
+//            count++;
+//            if (rsl != 0 || count == first.size() || count == second.size()) {
+//                break;
+//            }
+//        } while (a);
         int rsl = 0;
-        List<Character> first = new ArrayList<>();
-        for(Character firstInvite : left.toCharArray()) {
-            first.add(firstInvite);
-        }
-        List<Character> second = new ArrayList<>();
-        for(Character secondInvite : right.toCharArray()) {
-            second.add(secondInvite);
-        }
-        do {
-            rsl = Character.compare(first.get(count),second.get(count));
-            count++;
-            if (rsl != 0 || count == first.size() || count == second.size()) {
-                break;
+        int min = Math.min(left.length(), right.length());
+        for (int x = 0; x < min; x++) {
+            rsl = Character.compare(left.charAt(x), right.charAt(x));
+            if (rsl != 0) {
+                return rsl;
             }
-        } while (a);
-        return rsl;
+        }
+        return Integer.compare(left.length(), right.length());
     }
 }
