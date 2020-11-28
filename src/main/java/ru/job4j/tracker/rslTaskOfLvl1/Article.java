a38c42deea35d0e3e5a6120e0085181ec16ba457
Just1lz <exelent.pride@rambler.ru>
2020-11-28 20:34:23 +0700

    Контрольные вопросы. [#306883]

--- src/main/java/ru/job4j/tracker/rslTaskOfLvl1/Article.java
+++ src/main/java/ru/job4j/tracker/rslTaskOfLvl1/Article.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 public class Article {
     public static boolean generateBy(String originText, String duplicateText) {
         boolean rsl = true;
-        String[] origin = originText.split(" |, |!|\\. ");
+        String[] origin = originText.split(" |\\p{P}");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>(Arrays.asList(origin));
         for (String fin : text) {
