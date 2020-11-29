efc02366fbae946f2319a9b6f3b6b1baf9e87d85
Just1lz <exelent.pride@rambler.ru>
2020-11-29 15:32:08 +0700

    1. Экзамен. Базовый синтаксис. [#306858]

--- src/main/java/ru/job4j/tracker/rslTaskOfLvl1/Merge.java
+++ src/main/java/ru/job4j/tracker/rslTaskOfLvl1/Merge.java
@@ -2,38 +2,17 @@ package ru.job4j.tracker.rslTaskOfLvl1;
 
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
-        int tmp = 0;
-        int min = 0;
-        int middle = 0;
-        int max = 0;
         int[] rsl = new int[left.length + right.length];
-        for(int i = 0; i < rsl.length; i++) {
-            if (rsl[rsl.length - 1] != 0) {
-                break;
-            }
-            if (i < left.length && i < right.length) {
-                if (rsl[i] == 0) {
-                    rsl[i] = Math.min(left[i], right[i]);
-                    rsl[i + 1] = Math.max(left[i], right[i]);
-                } else {
-                    min = rsl[i] < left[i] && rsl[i] < right[i] ? rsl[i] :
-                            left[i] < rsl[i] && left[i] < right[i] ? left[i] : right[i];
-                    middle = (rsl[i] > left[i] && rsl[i] < right[i]) ||
-                            (rsl[i] < left[i] && rsl[i] > right[i]) ? rsl[i] :
-                            (left[i] > rsl[i] && left[i] < right[i]) ||
-                                    (left[i] < rsl[i] && left[i] > right[i]) ?
-                                    left[i] : right[i];
-                    max = rsl[i] > left[i] && rsl[i] > right[i] ? rsl[i] :
-                            left[i] > rsl[i] && left[i] > right[i] ? left[i] : right[i];
-                    rsl[i] = min;
-                    rsl[i+1] = middle;
-                    rsl[i+2] = max;
-                }
-            } else if (i < left.length) {
-                rsl[i] = left[i];
-            } else  {
-                tmp = i - left.length;
-                rsl[i] = right[tmp];
+        int leftPoint = 0;
+        int rightPoint = 0;
+        int rslPoint = 0;
+        while (leftPoint + rightPoint != rsl.length) {
+            if (leftPoint != left.length && rightPoint != right.length) {
+                rsl[rslPoint++] = left[leftPoint] < right[rightPoint] ? left[leftPoint++] : right[rightPoint++];
+            } else if (leftPoint != left.length) {
+                rsl[rslPoint++] = left[leftPoint++];
+            } else {
+                rsl[rslPoint++] = right[rightPoint++];
             }
         }
         return rsl;
